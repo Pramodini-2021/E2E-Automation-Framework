63af97a855f299b9a159d15e0509040a2d97848c
pramodini g <pramodinice@gmail.com>
2026-07-10 00:15:00 +0530

    Initial project setup with Maven and base driver config

---
+++ src/main/java/org/example/utils/DriverFactory.java
@@ -0,0 +1,33 @@
+package org.example.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver initDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver.set(new ChromeDriver(options));
+        }
+        return getDriver();
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (getDriver() != null) {
+            getDriver().quit();
+            driver.remove();
+        }
+    }
+}
+
