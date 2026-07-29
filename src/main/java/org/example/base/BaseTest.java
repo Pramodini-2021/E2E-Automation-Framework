a148d50a770d9cefcfc55b1348410aea9dee69fc
pramodini g <pramodinice@gmail.com>
2026-07-29 19:15:52 +0530

    Integrate Extent Reports with failure screenshots

--- src/main/java/org/example/base/BaseTest.java
+++ src/main/java/org/example/base/BaseTest.java
@@ -1,23 +1,59 @@
 package org.example.base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.apache.commons.io.FileUtils;
 import org.example.utils.ConfigReader;
 import org.example.utils.DriverFactory;
+import org.example.utils.ExtentManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected static ExtentReports extent = ExtentManager.getInstance();
+    protected ExtentTest test;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp(java.lang.reflect.Method method) {
         driver = DriverFactory.initDriver(ConfigReader.get("browser"));
         driver.get(ConfigReader.get("url"));
+        test = extent.createTest(method.getName());
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            String screenshotPath = captureScreenshot(result.getName());
+            test.log(Status.FAIL, "Test Failed: " + result.getThrowable());
+            test.addScreenCaptureFromPath(screenshotPath);
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, "Test Passed");
+        }
+
         DriverFactory.quitDriver();
+        extent.flush();
+    }
+
+    private String captureScreenshot(String testName) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        String path = "test-output/screenshots/" + testName + ".png";
+        File destination = new File(path);
+        try {
+            FileUtils.copyFile(source, destination);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save screenshot", e);
+        }
+        return path;
     }
 }
