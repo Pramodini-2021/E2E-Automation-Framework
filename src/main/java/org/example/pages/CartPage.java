431a16f70ae8a0c903e6e431863d0f69b8e898af
pramodini g <pramodinice@gmail.com>
2026-07-30 17:32:18 +0530

    Add explicit wait for checkout button to fix CI flakiness

--- src/main/java/org/example/pages/CartPage.java
+++ src/main/java/org/example/pages/CartPage.java
@@ -1,26 +1,27 @@
 package org.example.pages;
 
+import org.example.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CartPage {
 
     private WebDriver driver;
+    private WaitUtils waitUtils;
 
-    // Locators
     private By cartItemName = By.className("inventory_item_name");
     private By checkoutButton = By.id("checkout");
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+        this.waitUtils = new WaitUtils(driver);
     }
 
-    // Actions
     public String getCartItemName() {
         return driver.findElement(cartItemName).getText();
     }
 
     public void clickCheckout() {
-        driver.findElement(checkoutButton).click();
+        waitUtils.waitForClickable(checkoutButton).click();
     }
 }
